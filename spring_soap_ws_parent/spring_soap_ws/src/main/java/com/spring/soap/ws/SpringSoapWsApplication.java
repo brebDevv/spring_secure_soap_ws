b2b4d2e9367554dfaf2115a3dd37e96e4e943214
Luigi <gava.luigi62n64@yahoo.com>
2016-03-07 21:24:37 +0200

    First commit

---
+++ spring_soap_ws_parent/spring_soap_ws/src/main/java/com/spring/soap/ws/SpringSoapWsApplication.java
@@ -0,0 +1,12 @@
+package com.spring.soap.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapWsApplication.class, args);
+	}
+}
---
+++ spring_soap_ws_parent/spring_soap_ws_xsd/src/main/java/com/soap/ws/xsd/SpringSoapWsXsdApplication.java
@@ -0,0 +1,12 @@
+package com.soap.ws.xsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapWsXsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapWsXsdApplication.class, args);
+	}
+}
