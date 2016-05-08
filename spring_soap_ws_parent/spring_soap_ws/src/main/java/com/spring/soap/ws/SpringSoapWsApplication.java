0fa7fe11ca81ff7a9118f348ca0bf3ff98adfe25
Luigi <gava.luigi62n64@yahoo.com>
2016-05-09 00:07:06 +0300

    Made spring ws project runnable from an external tomcat.

--- spring_soap_ws_parent/spring_soap_ws/src/main/java/com/spring/soap/ws/SpringSoapWsApplication.java
+++ spring_soap_ws_parent/spring_soap_ws/src/main/java/com/spring/soap/ws/SpringSoapWsApplication.java
@@ -1,10 +1,15 @@
 package com.spring.soap.ws;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class SpringSoapWsApplication {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class SpringSoapWsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSoapWsApplication.class, args);
